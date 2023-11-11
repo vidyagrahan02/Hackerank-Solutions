94ab7b82d702a3a0c4ff798a495c526723910cfe
vidyagrahan02 <150534113+vidyagrahan02@users.noreply.github.com>
2023-11-11 21:01:26 +0530

    Update 01 - Say 'Hello, World!' With Python.py

--- Python/01 - Introduction/01 - Say 'Hello, World!' With Python.py
+++ Python/01 - Introduction/01 - Say 'Hello, World!' With Python.py
@@ -1,8 +1,3 @@
-# ========================
-#       Information
-# ========================
-
-# Direct Link: https://www.hackerrank.com/challenges/py-hello-world/problem
 # Difficulty: Easy
 # Max Score: 5
 # Language: Python
